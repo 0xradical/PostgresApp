2b515df9a8f557c44198558c40c8b9d29121d5bc
Thiago Brandão <thiagobrandam@gmail.com>
2014-12-31 02:15:01 -0200

    Shows tables in sidebar

---
+++ TablesController.h
@@ -0,0 +1,13 @@
+//
+//  TablesController.h
+//  PostgresApp
+//
+//  Created by thiago on 12/31/14.
+//  Copyright (c) 2014 thiagobrandam. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface TablesController : NSViewController<NSTableViewDataSource>
+
+@end
---
+++ TablesController.m
@@ -0,0 +1,62 @@
+//
+//  TablesController.m
+//  PostgresApp
+//
+//  Created by thiago on 12/31/14.
+//  Copyright (c) 2014 thiagobrandam. All rights reserved.
+//
+
+#import "TablesController.h"
+#import "DBWindowController.h"
+#import "PGConnection.h"
+#import "PGResult.h"
+
+@interface TablesController ()
+{
+    PGResult *_result;
+}
+@property (weak) IBOutlet DBWindowController *dbWindowController;
+@property (weak) IBOutlet NSTableView *tables;
+
+@end
+
+@implementation TablesController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+    
+}
+
+- (void)awakeFromNib
+{
+    _result = [[self connection] execute:@"SELECT table_name FROM information_schema.tables WHERE table_schema = 'public' ORDER BY table_name"];
+    
+    [[self tables] reloadData];
+   
+}
+
+#pragma mark -
+#pragma mark NSTableViewDataSource
+
+- (NSInteger)numberOfRowsInTableView:(NSTableView *)tableView
+{
+    return [_result rowsCount];
+}
+
+#pragma mark NSTableViewDelegate
+
+- (id)tableView:(NSTableView *)aTableView objectValueForTableColumn:(NSTableColumn *)aTableColumn row:(NSInteger)rowIndex
+{
+    return [_result valueForRow:rowIndex AndColumn:0];
+}
+
+#pragma mark -
+#pragma Methods delegated to DBWindowController
+
+- (PGConnection *)connection
+{
+    return [[self dbWindowController] connection];
+}
+
+@end
