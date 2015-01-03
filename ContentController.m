c391ca323d4803f443504f1672f31d46cfaf209c
Thiago Brandão <thiagobrandam@gmail.com>
2015-01-03 11:28:44 -0200

    Adds previous / next buttons

--- ContentController.m
+++ ContentController.m
@@ -15,16 +15,21 @@
 @interface ContentController ()
 {
     PGResult *_result;
+    NSString *_currentTable;
+    NSUInteger _offset;
 }
 @property (weak) IBOutlet DBWindowController *dbWindowController;
 
 @property (weak) IBOutlet NSTableView *content;
+@property (weak) IBOutlet NSButton *previous;
+@property (weak) IBOutlet NSButton *next;
 
 @end
 
 @implementation ContentController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
     // Do view setup here.
 }
@@ -39,29 +44,37 @@
 
 - (void)loadCurrentTableData:(NSNotification *)aNotification
 {
-    NSString *query = [NSString stringWithFormat:@"SELECT * FROM %@ LIMIT 20", [aNotification object]];
+    _currentTable = [aNotification object];
+    _offset = 0;
+        
+    NSString *query = [NSString stringWithFormat:@"SELECT * FROM %@ OFFSET %@ LIMIT 20", _currentTable, @(_offset)];
     
-    _result = [[self connection] execute:query];
+    [self loadContentByQuery:query];
     
-    NSArray *columns = [[_content tableColumns] copy];
-    
-    for( int i=0; i < [columns count]; i++)
-    {
-        NSTableColumn *col = [columns objectAtIndex:i];
-        [_content removeTableColumn:col];
-    }
-    
-    for( int i=0; i < [_result fieldsCount]; i++)
-    {
-        NSTableColumn *column = [[NSTableColumn alloc] initWithIdentifier:[NSString stringWithFormat:@"%i", i]];
-        [column setEditable:NO];
-        [[column headerCell] setStringValue:[_result fieldForColumn:i]];
-        [_content addTableColumn:column];
-    }
-    
-    [[_content headerView] setNeedsDisplay:YES];
-    [_content reloadData];
 }
+
+#pragma mark -
+#pragma Previous and Next mechanisms
+
+- (IBAction)fetchPrevious:(id)sender
+{
+    _offset = _offset - 20;
+    
+    NSString *query = [NSString stringWithFormat:@"SELECT * FROM %@ OFFSET %@ LIMIT 20", _currentTable, @(_offset)];
+    
+    [self loadContentByQuery:query];
+}
+
+- (IBAction)fetchNext:(id)sender
+{
+    _offset = _offset + 20;
+    
+    NSString *query = [NSString stringWithFormat:@"SELECT * FROM %@ OFFSET %@ LIMIT 20", _currentTable, @(_offset)];
+    
+    [self loadContentByQuery:query];
+    
+}
+
 
 #pragma mark -
 #pragma mark NSTableViewDataSource
@@ -84,6 +97,33 @@
 - (PGConnection *)connection
 {
     return [[self dbWindowController] connection];
+}
+
+#pragma mark -
+#pragma Auxiliary methods
+
+- (void)loadContentByQuery:(NSString *)query
+{
+    _result = [[self connection] execute:query];
+    
+    NSArray *columns = [[_content tableColumns] copy];
+    
+    for( int i=0; i < [columns count]; i++)
+    {
+        NSTableColumn *col = [columns objectAtIndex:i];
+        [_content removeTableColumn:col];
+    }
+    
+    for( int i=0; i < [_result fieldsCount]; i++)
+    {
+        NSTableColumn *column = [[NSTableColumn alloc] initWithIdentifier:[NSString stringWithFormat:@"%i", i]];
+        [column setEditable:NO];
+        [[column headerCell] setStringValue:[_result fieldForColumn:i]];
+        [_content addTableColumn:column];
+    }
+    
+    [[_content headerView] setNeedsDisplay:YES];
+    [_content reloadData];
 }
 
 @end
