f2d52a86e0ec0dbe2931bbc124e008047bd1670e
Thiago Brandão <thiagobrandam@gmail.com>
2015-01-04 00:33:52 -0200

    Loads structure

--- StructureController.m
+++ StructureController.m
@@ -7,16 +7,72 @@
 //
 
 #import "StructureController.h"
+#import "DBWindowController.h"
+#import "Notifications.h"
+#import "PGConnection.h"
+#import "PGResult.h"
 
 @interface StructureController ()
+{
+    PGResult *_result;
+    NSString *_currentTable;
+}
+@property (weak) IBOutlet DBWindowController *dbWindowController;
+@property (weak) IBOutlet NSTableView *structure;
 
 @end
 
 @implementation StructureController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
     // Do view setup here.
+}
+
+- (void)awakeFromNib
+{
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(loadCurrentTableStructure:) name:kTableCellWasSelected object:nil];
+}
+
+#pragma mark -
+#pragma Notification Center
+
+- (void)loadCurrentTableStructure:(NSNotification *)aNotification
+{
+    _currentTable = [aNotification object];
+    
+    NSString *query = [NSString stringWithFormat:@"SELECT column_name, data_type, character_maximum_length FROM information_schema.columns WHERE table_name = '%@'", _currentTable];
+    
+    _result = [[self connection] execute:query];
+    
+    [_structure reloadData];
+    
+}
+
+#pragma mark -
+#pragma mark NSTableViewDataSource
+
+- (NSInteger)numberOfRowsInTableView:(NSTableView *)tableView
+{
+    return [_result rowsCount];
+}
+
+#pragma mark NSTableViewDelegate
+
+// Getter
+- (id)tableView:(NSTableView *)aTableView objectValueForTableColumn:(NSTableColumn *)aTableColumn row:(NSInteger)rowIndex
+{
+    return [_result valueForRow:rowIndex AndColumn:[[aTableColumn identifier] intValue]];
+}
+
+
+#pragma mark -
+#pragma Methods delegated to DBWindowController
+
+- (PGConnection *)connection
+{
+    return [[self dbWindowController] connection];
 }
 
 @end
