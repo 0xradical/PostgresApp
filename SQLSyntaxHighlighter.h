734b7df0b95b567ea16a916797e2c3ca8c5fcab9
Thiago Brandão <thiagobrandam@gmail.com>
2015-01-18 00:50:34 -0200

    Adds SyntaxHighliter from mattt’s Induction

---
+++ SQLSyntaxHighlighter.h
@@ -0,0 +1,13 @@
+//
+//  SQLSyntaxHighlighter.h
+//  PostgresApp
+//
+//  Created by thiago on 1/18/15.
+//  Copyright (c) 2015 thiagobrandam. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SQLSyntaxHighlighter : NSObject <NSTextViewDelegate, NSTextStorageDelegate>
+
+@end
